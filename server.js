fbd611ebd5fca7e787fbc255dd96cd208cbae1fb
Galina Gatalskaya <gatalskaya.cniitu@gmail.com>
2024-12-02 14:48:57 +0300

    with mysql.createConnection
    with mysql.createConnection

--- server.js
+++ server.js
@@ -3,39 +3,38 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const mysql = require('mysql');
 
-const connectionConfig = {
+const poolConfig = {
+  connectionLimit: 2,
   host: 'localhost',
   user: 'root',
   password: '1234',
   database: 'learning_db',
 };
+let pool = mysql.createPool(poolConfig);
 
 const webserver = express();
 const port = 7380;
 
-// webserver.use(bodyParser.json());
 webserver.use(bodyParser.text({}));
 webserver.use(express.static(path.resolve(__dirname, 'public')));
 
 webserver.post('/', (req, res) => {
-  try {
-    connection = mysql.createConnection(connectionConfig);
-    connection.connect();
-    connection.query(`${req.body};`, (error, results, fields) => {
-      if (error) {
-        res
-          .status(500)
-          .send(error)
-          .end();
-      } else {
-        res.send(results);
-      }
-      connection.end();
-    });
-  } catch (error) {
-    res.status(500).end();
-    if (connection) connection.end();
-  }
+  pool.getConnection((err, connection) => {
+    try {
+      connection.query(`${req.body};`, (error, results) => {
+        if (error) {
+          res.status(500).send(error);
+          connection.release();
+        } else {
+          console.log('results', results[0]);
+          res.send(results);
+        }
+      });
+    } catch (err) {
+      res.status(500).end();
+      connection.release();
+    }
+  });
 });
 
 webserver.listen(port, () => console.log('webserver running on port ' + port));
